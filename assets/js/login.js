b8930381a2c925e6059d747996b59fd7c88ffc6c
tansheng <923149151@qq.com>
2020-10-10 11:29:26 +0800

    完成了密码长度设置

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,13 @@ $(function () {
         $('.login-box').show()
         $('.reg-box').hide()
     })
+    //从layui上获取form对象
+    let form = layui.form
+    //通过form.verify()函数自定义校验规则
+    form.verify({
+        //自定义了一个叫做pwd校验规则
+        pwd: [/^[\s]{6,12}$/, '必须是6到12位,不能为空格']
+    })
+
+
 })
