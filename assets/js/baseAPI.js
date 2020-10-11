04e8c014970f330fddd0822666200aacb9e46a68
tansheng <923149151@qq.com>
2020-10-11 09:56:46 +0800

    后台完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,6 +4,12 @@
 $.ajaxPrefilter(function (options) {
     console.log(options.url);
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+    //方便随时更改根目录
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
-//方便随时更改根目录
